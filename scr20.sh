daaeeac458a18951d3064900b893d891b1b56ff2
donesii <bond.007.911@mail.ru>
2020-12-04 21:07:55 +0300

    Ver.final

--- scr20.sh
+++ scr20.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-count=1
-for i in "$@"
-do 
-echo "parametr $count = $i"
-count=$(($count+1))
-done
+echo "4 10 21
+6 20 31 "\
+| awk '{s += $1 ; u += $2 ; m += $3; ave1= s/2; ave2=u/2; ave3=m/2 } END  {print s,u,m,  ave1,ave2,ave3} '
+
